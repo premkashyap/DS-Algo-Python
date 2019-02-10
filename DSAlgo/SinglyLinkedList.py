979b8b0ddd1f4f0642f4ed39875a4360326df530
Prem Kashyap <pkashyap02@pc.factset.com>
2019-02-10 20:38:22 +0530

    Fixing SinglyLinkedList

--- DSAlgo/SinglyLinkedList.py
+++ DSAlgo/SinglyLinkedList.py
@@ -1,72 +1,83 @@
 class SinglyLinkedListNode:
-    def __init__(self, data=None, next_node=None):
+    def __init__(self, data=None, next=None):
        self.data = data
-       self.next_node = next_node
+       self.next = next
     
     def __str__(self):
         return str(self.data)
 
 
-class BaseLinkedList:
+class SinglyLinkedList:
     def __init__(self, head=None):
-            self.head = head
+        self.head = head
 
     def insert_start(self, data):
         new_node = SinglyLinkedListNode(data)
-        new_node.next_node = self.head
+        new_node.next = self.head
         self.head = new_node
 
-    def insert_end(self, data):
+    def insert(self, data):
         new_node = SinglyLinkedListNode(data)
         if self.head == None:
             self.head = new_node
             return
         node = self.head
-        while node.next_node !=None:
-            node = node.next_node
-        node.next_node=new_node
-
-    def insert_node_end(self, node):
-        if self.head == None:
-            self.head = node
-            return
-        current_node = self.head
-        while current_node.next_node !=None:
-            current_node = current_node.next_node
-        current_node.next_node=node
-    
+        while node.next !=None:
+            node = node.next
+        node.next=new_node
 
     def delete_start(self):
+        if self.head is None:
+            return
         temp = self.head
-        self.head= self.head.next_node
+        self.head= self.head.next
         return temp
     
     def delete_end(self):
-        if self.head ==None:
+        if self.head == None:
             return
         node = self.head
-        if self.head.next_node== None:
-            self.head = None
+        previous = None
+        while node.next is not None:
+            previous = node
+            node = node.next
+        if previous is not None:
+            previous.next = node.next
+        else:
+            self.head = node.next
+        return node
+        
+    def delete(self, data):
+        if self.head is None:
             return
-        while node.next_node.next_node is not None:
-            node = node.next_node
-        temp = node.next_node
-        node.next_node= None
-        return temp
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        node = self.head
+        previous = None
+        while node is not None:
+            if node.data == data:
+                break
+            previous = node
+            node = node.next
+        if node is None:
+            return
+        previous.next = node.next
+            
         
     def __str__(self):
         node = self.head
         st = ''
         while node !=None:
             st += str(node.data) + ' '
-            node = node.next_node
+            node = node.next
         return st
 
     @classmethod
     def create_from_list(cls, lst):
-        lnklst =cls()
+        lnklst = cls()
         for data in lst:
-            lnklst.insert_end(data)
+            lnklst.insert(data)
         return lnklst
 
     def __iter__(self):
@@ -85,79 +96,56 @@ class BaseLinkedList:
         return False
 
     def __len__(self):
-        i =0
+        i=0
         node = self.head
         while node is not None:
             i+=1
-            node = node.next_node
+            node = node.next
         return i
         
-class SinglyLinkedList(BaseLinkedList):
-
-    def delete_value(self, value):
-        current = self.head
-        previous = None
-        while current is not None:
-            if current.data == value:
-                if previous is not None:
-                    previous.next_node = current.next_node
-                    return True
-            previous = current
-            current = current.next_node
-    
-    def delete_node(self, node):
-        current = self.head
-        previous = None
-        while current is not None:
-            if current == node:
-                if previous is not None:
-                    previous.next_node = current.next_node
-                    return True
-            previous = current
-            current = current.next_node
-
-
     def reverse(self):
         current = self.head
         previous = None
         while current is not None:
-            next = current.next_node
-            current.next_node = previous
+            next = current.next
+            current.next = previous
             previous = current
             current = next
         self.head = previous
     
     def find_midpoint(self):
-        slow = self.head
-        fast = self.head
-        while fast is not None and fast.next_node is not None:
-            slow = slow.next_node
-            fast = fast.next_node.next_node
+        slow = fast = self.head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
         return slow
 
     def nth_node_from_end(self, n):
-        i =0 
+        i = 0 
         node = self.head
         while node is not None and i <= n:
             i+=1
-            node = node.next_node
+            node = node.next
             if node is None:
-                return None
+                return
         behind = self.head
         while node is not None:
-            node = node.next_node
-            behind = behind.next_node
+            node = node.next
+            behind = behind.next
         return behind
     
     def delete_dupes(self):
         dictionary = {}
         node = self.head
+        previous = None
         while node is not None:
             if node.data in dictionary.keys():
-                self.delete_node(node)
+                previous.next = node.next
             else:
                 dictionary[node.data] = None
-            node = node.next_node
+            previous = node
+            node = node.next
+           
 
 def sort_list_inplace_with3elements(lst):
     lst0, lst1, lst2 = [SinglyLinkedList() for _ in range(3)]
@@ -165,7 +153,8 @@ def sort_list_inplace_with3elements(lst):
     print(lst0)
     print(lst1)
     print(lst2)
+
 if __name__ == '__main__':
-    lst = SinglyLinkedList()
-    temp = SinglyLinkedList.create_from_list([1,0,1,2,2,1,1,0])
-    sort_list_inplace_with3elements(temp)
+    lst = SinglyLinkedList.create_from_list([1,2,3,4,5,6])
+    print(lst.find_midpoint())
+    print(len(lst))
