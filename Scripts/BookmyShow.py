7d2ae58404ab53162366c4a70a110d96a294b23d
Prem Kashyap <pkashyap@ITSupports-MacBook-Pro.local>
2019-04-19 22:48:55 +0530

    Adding multiples showids

--- Scripts/BookmyShow.py
+++ Scripts/BookmyShow.py
@@ -7,7 +7,7 @@ import datetime
 venue_code ='AMBH' #this can be found by inspecting the element data-id for the venue where you would like to watch
 date = '20190426'
 venue_name = 'amb-cinemas-gachibowli'
-show_id = 'ET00100559'
+show_ids = ['ET00100559', 'ET00090482']
 delay=300 #timegap in seconds between 2 script runs
 
 TO = 'kashyap18.prem@gmail.com' #mail id for which you want to get alerted
@@ -37,11 +37,11 @@ def is_show_available(venue_name, venue_code, date, show_id):
     req = urllib.request.Request(f"https://in.bookmyshow.com/buytickets/{venue_name}/cinema-hyd-{venue_code}-MT/{date}")
     page = urllib.request.urlopen(req)
     soup = BeautifulSoup(page)
-    return show_id in str(soup)
+    return any(show_id in str(soup) for show_id in show_ids)
 
 count = 0
 while True:
-    if is_show_available(venue_name, venue_code, date, show_id):
+    if is_show_available(venue_name, venue_code, date, show_ids):
         print("Available")
         if count < 10:
             send_email(GMAIL_USER, GMAIL_PASS, SUBJECT, TEXT, TO)
